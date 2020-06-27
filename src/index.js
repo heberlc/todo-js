51311f3d426999952f4dd450160688dadba8d7f6
Hebert <hlavadoc@gmail.com>
2020-06-26 23:38:18 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+export const todoList = new TodoList();
+
+// todoList.todos.forEach((todo) => crearTodoHtml(todo));
+todoList.todos.forEach(crearTodoHtml);
+/*
+const newTodo = new Todo('Aprender JavaScript');
+
+todoList.todos[0].imprimirClase(); */
+
+console.log('todos', todoList.todos);
+/* const tarea = new Todo('Aprender Javascript!');
+
+todoList.nuevoTodo(tarea);
+
+crearTodoHtml(tarea); */
+
+/* localStorage.setItem('mi-key', 'ABC1234');
+sessionStorage.setItem('mi-key', 'ABC1234');
+ */
+/* setTimeout(() => {
+	localStorage.removeItem('mi-key');
+}, 1500); */
